8b4c5b1bf334b688b55c07b1530c07b04efff276
TAOBU <tao_bu@163.com>
2018-10-16 09:30:55 +0800

    test

--- src/main/java/com/example/demo/controller/demo.java
+++ src/main/java/com/example/demo/controller/demo.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class demo {
     @RequestMapping("login")
     public String hi(){
+        //测试
         return  "哈哈";
     }
 }
